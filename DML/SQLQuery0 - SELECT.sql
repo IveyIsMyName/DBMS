2ec0973a8e1610ba55b214fa092681ce36390eed
Ilyuh_dr01@top-academy.ru <Ilyuh_dr01@top-academy.ru>
2025-02-10 13:09:02 -0500

    INSERT/COUNT/SELECT DONE

---
+++ DML/SQLQuery0 - SELECT.sql
@@ -0,0 +1,13 @@
+USE PV_319_DDL;
+GO
+
+--SELECT 
+--		--last_name	AS	N'Фамилия',
+--		--first_name	AS	N'Имя',
+--		--middle_name AS	N'Отчество',
+--		last_name + ' ' + first_name + ' ' + middle_name AS N'Ф.И.О студента',
+--		birth_date	AS	N'Дата рождения',
+--		[group]		AS	N'Группа'
+--FROM Students;
+
+SELECT * FROM Teachers; 
