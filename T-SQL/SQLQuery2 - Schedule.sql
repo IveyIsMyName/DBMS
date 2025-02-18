8f1289505359904eae06b67e4b6e0d31ab839a74
Ilyuh_dr01@top-academy.ru <Ilyuh_dr01@top-academy.ru>
2025-02-18 10:20:21 -0500

    PrintScheduleByDates DONE

--- T-SQL/SQLQuery2 - Schedule.sql
+++ T-SQL/SQLQuery2 - Schedule.sql
@@ -1,8 +1,16 @@
 USE PV_319_Import;
 GO
 
-EXEC sp_SetScheduleForStacionarGroup N'PV_319', N'%UML%', N'Ковтун', N'2024-08-10', N'18:30';
+--EXEC sp_SetScheduleForStacionarGroup N'PV_319', N'%UML%', N'Ковтун', N'2024-08-10', N'18:30';
 
-EXEC sp_PrintScheduleForGroup N'PV_319', N'%UML%';
+--EXEC sp_PrintScheduleForGroup N'PV_319', N'%UML%';
+
+EXEC sp_PrintScheduleByDates
+N'PV_319',
+N'',
+'2024-06-06',
+'2024-08-08'
+
+
 
 --SELECT * FROM Schedule;
